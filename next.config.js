64baa2589b55facdf196118ca63d4b75496eb968
SaRTHaK0909k <lcb2022037@iiitl.ac.in>
2024-12-25 19:31:51 +0530

    refactor: deployment  changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Ignore ESLint during the build process
   },
-  images: { unoptimized: true },
-  
+  images: {
+    unoptimized: true, // Disables Next.js image optimization (useful for external images)
+  },
+  env: {
+    // Environment variables for use in your Next.js project
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://neondb_owner:L8NTbQuAf0qK@ep-square-block-a5gwsapk.us-east-2.aws.neon.tech/neondb?sslmode=require',
+  },
+  webpack: (config) => {
+    // Modify webpack configuration if needed
+    return config;
+  },
 };
 
 module.exports = nextConfig;
